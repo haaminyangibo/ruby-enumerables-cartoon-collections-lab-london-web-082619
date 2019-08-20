63eaef2c2ba31643f3537f55a4d5c4df370db158
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-20 11:35:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -43,8 +43,6 @@ long_planeteer_calls(call_screams)
 cheese_types = ["cheddar", "gouda", "camembert"]
 snacks = ["crackers", "gouda", "thyme"]
 
-
-# This is the learn.co method that wants the cheese array inside the actual method
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
@@ -54,9 +52,6 @@ end
 
 find_the_cheese(snacks)
 
-
-
-# This is a much better way cause the cheese array isn't included in the method.
 cheese_types = ["cheddar", "gouda", "camembert"]
 snacks = ["crackers", "gouda", "thyme"]
 
